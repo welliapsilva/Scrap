4eb83b339e8e8594fdc8424402d755e98b42f149
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-09-01 12:13:41 -0300

    ajuste para extrair informaçõe da pagina

--- varredor_de_site/varredor_de_site/spiders/quotesToScrapy.py
+++ varredor_de_site/varredor_de_site/spiders/quotesToScrapy.py
@@ -15,5 +15,16 @@ class QuotesToScrapeSpider(scrapy.Spider):  #colocar o nome da classe e entre pa
     #Response
     def parse(self,response):
         #aqui é  onde voce deve processar o qeu é retornado da response
-        with open('pagina.html', 'wb') as arquivo: #retornando o arquivo com os dados do html
-            arquivo.write(response.body)
+
+    ##este trecho copia o html de uma pgina
+        # with open('pagina.html', 'wb') as arquivo: #retornando o arquivo com os dados do html
+        #     arquivo.write(response.body)  
+        for elemento in response.xpath('//div[@class="quote"]'): ##encontra as 10 citaçoes que este site tem
+            yield{
+                'frase': elemento.xpath('.//span[@class="text"]/text()').get(), # retorna a frase do primeio elemento enocntrado dentro da div da frase
+                'autor': elemento.xpath('.//small[@class="author"]/text()').get(), #retorno o autor da frase acima selecionada
+                'tags':elemento.xpath('.//a[@class="tag"]/text()').getall() #retorna todas as tags de de cada citação
+
+            }
+
+##colocando o ponto antes do xpath que são referentes ao primeiro, isso indita que ao voltar o laço de repetiação ele deve coltar somente na div espeficada no elemento pai.
